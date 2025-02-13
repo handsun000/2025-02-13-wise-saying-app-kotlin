833ba74276dcb9e41a01ba181ee2219737feb2e9
son <handsun000@gmail.com>
2025-02-13 15:23:12 +0900

    work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,36 @@
 package com.ll
 
+import java.util.Objects
 import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
     println("== 명언 앱 ==")
+
     var idx = 0;
+    val list = mutableListOf<Map<String, Any>>()
 
     while(true) {
         print("명령) ")
         val command = scanner.next()
         if ("종료" == command) break;
         else if ("등록" == command) {
+            val map = mutableMapOf<String, Any>()
             print("명언 : ")
-            scanner.next()
+            map.put("content", scanner.next())
             print("작가 : ")
-            scanner.next()
+            map.put("author", scanner.next())
             idx++;
+            map.put("id", idx)
+            list.add(map)
             println("%d번 명언이 등록되었습니다.".format(idx))
+        }
+        else if ("목록" == command) {
+            println("번호 / 작가 / 명언")
+            println("----------------------")
+            for (data in list) {
+                println("${data["id"]} / ${data["author"]} / ${data["content"]}")
+            }
         }
     }
 }
